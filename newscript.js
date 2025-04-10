d890bef385dc97ceb2ba28a9d835e1ee8e344471
priya <uppleria@gmail.com>
2025-04-10 15:24:01 -0500

    Updated JavaScript logic in script.js

--- newscript.js
+++ newscript.js
@@ -1,6 +1,6 @@
 // Part 1: Creating and using an object
 const student = {
-  name: 'Alex Johnson',
+  name: 'priya upple',
   age: 21,
   enrolled: true,
   courses: ['Math', 'Physics', 'Computer Science'],
@@ -13,14 +13,14 @@ console.log("Name:", student.name);
 console.log("Age:", student.age);
 console.log("Display Info:", student.displayInfo());
 
-// Part 2: Working with JSON
+
 const jsonString = JSON.stringify(student);
 console.log("JSON String:", jsonString);
 
 const parsedStudent = JSON.parse(jsonString);
 console.log("Parsed Object:", parsedStudent);
 
-// Part 3: Destructuring
+
 const { name, courses } = student;
 console.log("Destructured Name:", name);
 console.log("Destructured Courses:", courses);
@@ -30,7 +30,7 @@ const [score1, score2] = scores;
 console.log("Score 1:", score1);
 console.log("Score 2:", score2);
 
-// Part 4: Spread Operator
+
 const clonedStudent = { ...student, graduationYear: 2025 };
 console.log("Cloned Student with Graduation Year:", clonedStudent);
 
@@ -38,7 +38,7 @@ const newCourses = ['Art', 'History'];
 const allCourses = [...student.courses, ...newCourses];
 console.log("All Courses Combined:", allCourses);
 
-// Part 5: Object Methods
+
 student.addCourse = function(course) {
   this.courses.push(course);
 };
